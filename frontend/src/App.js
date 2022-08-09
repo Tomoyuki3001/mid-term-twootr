8a147dd7dbc94fa611b024553ce81738e3d15375
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-08-09 10:36:23 -0700

    removed twootslist css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import MainComponents from "./components/Main";
 
 function App() {
--- frontend/src/components/styles/TwootsList.js
+++ frontend/src/components/styles/TwootsList.js
@@ -1,56 +1,55 @@
 import styled from "styled-components";
 
-export const TwootsList = styled.div`
-  div {
-    background-color: #eef0f2;
-  }
-  .account-form {
-    background-color: black;
-  }
-  .post_user_box {
-    padding: 10px;
-    margin: 5% 10%;
-    border: 2px solid black;
-    img {
-      max-width: 60px;
-    }
-  }
+export const TwootsList = styled.div``;
+// div {
+//   background-color: #eef0f2;
+// }
+// .account-form {
+//   background-color: black;
+// }
+// .post_user_box {
+//   padding: 10px;
+//   margin: 5% 10%;
+//   border: 2px solid black;
+//   img {
+//     max-width: 60px;
+//   }
+// }
 
-  .post_user_box {
-    padding: 20px;
-    cursor: -webkit-grab;
-    transition: all 0.5s;
-    :hover {
-      transition: all 0.5s;
-      -webkit-box-shadow: 6px 5px 0px 0px rgba(0, 0, 0, 0.4);
-      box-shadow: 6px 5px 0px 0px rgba(0, 0, 0, 0.4);
-    }
-  }
-  .post_user_information {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .post_user_date_icons {
-    display: flex;
-    justify-content: space-between;
-    cursor: pointer;
-    span {
-      margin-right: 10px;
-    }
-  }
-  .post_user_contents p {
-    border-bottom: 2px solid black;
-    padding-bottom: 4px;
-  }
-  .user-profile {
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    span {
-      min-width: 320px;
-      font-weight: bold;
-      margin-left: 10px;
-    }
-  }
-`;
+// .post_user_box {
+//   padding: 20px;
+//   cursor: -webkit-grab;
+//   transition: all 0.5s;
+//   :hover {
+//     transition: all 0.5s;
+//     -webkit-box-shadow: 6px 5px 0px 0px rgba(0, 0, 0, 0.4);
+//     box-shadow: 6px 5px 0px 0px rgba(0, 0, 0, 0.4);
+//   }
+// }
+// .post_user_information {
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+// }
+// .post_user_date_icons {
+//   display: flex;
+//   justify-content: space-between;
+//   cursor: pointer;
+//   span {
+//     margin-right: 10px;
+//   }
+// }
+// .post_user_contents p {
+//   border-bottom: 2px solid black;
+//   padding-bottom: 4px;
+// }
+// .user-profile {
+//   display: flex;
+//   align-items: center;
+//   flex-direction: row;
+//   span {
+//     min-width: 320px;
+//     font-weight: bold;
+//     margin-left: 10px;
+//   }
+// }
