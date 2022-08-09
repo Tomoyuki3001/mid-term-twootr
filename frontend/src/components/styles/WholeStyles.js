55c3051dea0f920adadd27c2ab41cc50fff8bda4
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-08-09 10:49:07 -0700

    div wholestyles css

--- frontend/src/components/styles/WholeStyles.js
+++ frontend/src/components/styles/WholeStyles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const WholeStyles = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap");
-  div {
-    margin: 0;
-    font-family: "Poppins", sans-serif;
-  }
 `;
+// div {
+//   margin: 0;
+//   font-family: "Poppins", sans-serif;
+// }
